42bf94a8a1e0e34500780332cfb64852d8df64c6
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-05-02 09:29:11 +0800

    making it work on a new flutter version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/philippmuellauer/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/philippmuellauer/Downloads/Dicee-Flutter-Completed"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\Users\ErwinMacaraig\development\sdks\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\ErwinMacaraig\development\projects\Dicee-Flutter-Completed"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/philippmuellauer/Developer/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
